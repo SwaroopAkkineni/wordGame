a6bcb8e9d58423b75f38c1cb094e010f41a15eb0
Swaroop Akkineni <swa7@pitt.edu>
2022-02-18 21:55:01 -0500

    000 basic setup

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(new File("./corpeus.txt"));
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
