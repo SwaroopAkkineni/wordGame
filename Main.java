f664eb0f284b58db916438465a2d5449ff5f69f4
Swaroop Akkineni <swa7@pitt.edu>
2022-02-18 22:08:46 -0500

    0.0.1 cleaner

--- Main.java
+++ Main.java
@@ -1,18 +1,30 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            Scanner scanner = new Scanner(new File("./corpeus.txt"));
-            while (scanner.hasNextLine()) {
-                System.out.println(scanner.nextLine());
-            }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        Set<String> words = parseWords();
+
+        for (String word : words) {
+            System.out.println(word);
         }
     }
 
+    private static Set<String> parseWords() {
+        Set<String> words = new HashSet<String>();
+
+        try {
+            Scanner scanner = new Scanner(new File("./corpeus.txt"));
+            while (scanner.hasNext()) {
+                words.add(scanner.next());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return words;
+    }
 }
