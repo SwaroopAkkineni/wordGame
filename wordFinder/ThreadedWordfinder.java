3d52eca75f0ca9f4804f69549174058541b717e4
Swaroop Akkineni <swa7@pitt.edu>
2022-02-19 21:53:58 -0500

    0.0.5 actually mulithreaded search

--- wordFinder/ThreadedWordfinder.java
+++ wordFinder/ThreadedWordfinder.java
@@ -4,8 +4,10 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -53,12 +55,26 @@ public class ThreadedWordfinder implements WordFinder {
         ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
         ThreadPoolExecutor pool = (ThreadPoolExecutor) executor;
 
+        List<Future<Boolean>> resultList = new ArrayList<>();
+
         for (int i = 0; i < THREAD_COUNT; i++) {
-            executor.submit(new Task(i, corpus, word));
+            // executor.submit(new Task(i, corpus, word));
+            Future<Boolean> result = executor.submit(new Task(i, corpus, word));
+            resultList.add(result);
         }
 
+        for (Future<Boolean> future : resultList) {
+            try {
+                if (future.get()) {
+                    executor.shutdown();
+                    return true;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        // shut down the executor service now
         executor.shutdown();
-
         return false;
     }
 
@@ -82,7 +98,7 @@ public class ThreadedWordfinder implements WordFinder {
         }
     }
 
-    static class Task implements Runnable {
+    static class Task implements Callable<Boolean> {
         int section;
         List<String> corpus;
         String word;
@@ -93,7 +109,7 @@ public class ThreadedWordfinder implements WordFinder {
             this.word = word;
         }
 
-        public void run() {
+        public Boolean call() throws Exception {
             try {
                 int onePart = corpus.size() / THREAD_COUNT;
                 int startIndex = section * onePart;
@@ -101,13 +117,15 @@ public class ThreadedWordfinder implements WordFinder {
 
                 for (int i = startIndex; i < endIndex; i++) {
                     if (corpus.get(i).equals(word)) {
-                        System.out.println("Found word: " + word + " in section: " + section);
-                        return;
+                        // System.out.println("Found word: " + word + " in section: " + section);
+                        return true;
                     }
                 }
 
+                return false;
             } catch (Exception e) {
                 e.printStackTrace();
+                return false;
             }
         }
     }
